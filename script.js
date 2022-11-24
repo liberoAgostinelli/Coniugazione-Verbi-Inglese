632959e6a1c1cbdece56fa9adaadc74c88d1427c
Libero Agostinelli <Libero@Agostinelli>
2022-11-24 11:35:22 +0100

    Prima versione del sito con header logo titolo e barra ricerca

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict"
+
+// const divApi = document.getElementById("api");
+
+// const i = document.getElementById("search");
+
+// const btn = document.getElementById("btn");
+
+// btn.addEventListener("click", call =>{
+//     let s = i.value;
+//     console.log(s);
+//     fetch(`https://lt-nlgservice.herokuapp.com/rest/english/conjugate?verb=${s}`)
+//     .then((res) => res.json()).then(data => divApi.innerText = JSON.stringify(data, null));
+// })
+
+// fetch('https://lt-nlgservice.herokuapp.com/rest/english/conjugate?verb=change')
+//     .then((res) => res.json()).then(data => console.log(data));
+
+const mySearch = document.getElementById("search");
+
+mySearch.addEventListener("input", i =>{
+    //console.log(i.target.value);
+    
+})
