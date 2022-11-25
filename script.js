af267a33bc8c5773b85cb146ef392ea949a2aff2
Libero Agostinelli <Libero@Agostinelli>
2022-11-25 10:49:43 +0100

    Aggiunta event listener dentro l event listener di search, per impostare l elemento cliccato a search. Aggiunta eventListener al bodi per chiudere lautocompletamento in qualsiasi punto della pagina. Aggiunta EventListener a btnImposta per impostare la parola scelta al div conjugate.

--- script.js
+++ script.js
@@ -19,24 +19,50 @@
 const mySearch = document.getElementById("search");
 const autoComplete = document.getElementById("autoComplete");
 
-mySearch.addEventListener("input", i =>{
+// Event listener che riceve i dati api da datamuse.com
+mySearch.addEventListener("input", i =>{    // Event listener per richiedere i dati ad ogni parola inserita
     //console.log(i.target.value);
-    autoComplete.innerHTML = '';
-    fetch(`https://api.datamuse.com/words?sp=${i.target.value}??`)
-    .then(res=> res.json())
-    .then(data => {
+    autoComplete.innerHTML = '';    // Cancellare lista parole
+    fetch(`https://api.datamuse.com/words?sp=${i.target.value}??`) // Richiesta al database delle parole del vocabolario
+    .then(res=> res.json())                                        // Trasformazione dei dati in oggetti
+    .then(data => {                 // CallBack per inserire i risultati nella lista 
         for(let {word} of data){
             console.log(word)
             const li = document.createElement('li');
+            li.addEventListener('click', e =>{  // Aggiunta event listener a tutti i li 
+                mySearch.value = e.target.outerText;
+                console.log(e.target.outerText);
+                
+            })
             li.innerText = word;
             autoComplete.appendChild(li);
+           
         }
         if(mySearch.value === '') autoComplete.innerHTML = '';
     });
     
 })
 
-// mySearch.addEventListener("keyup", e =>{
-//     console.log("tasto premuto")
-//     autoComplete.innerHTML = '';
-// })
+
+// Chiusura autoCompletamento ricerca quando si clicca in un qualsiasi punto della pagina
+
+const body = document.getElementsByTagName('body')[0];
+
+body.addEventListener('click', e =>{
+    console.log('Sono il body');
+    autoComplete.innerHTML = '';
+})
+
+// Impostare parola da coniugare
+
+const btnImposta = document.getElementById('btnImposta');
+const conjugate = document.getElementById('conjugate');
+
+btnImposta.addEventListener('click', e =>{
+    if(mySearch.value === '') conjugate.innerText = 'Conjugate => '
+    conjugate.innerText = 'Conjugate => ' + mySearch.value;
+    mySearch.value = '';
+})
+
+
+
